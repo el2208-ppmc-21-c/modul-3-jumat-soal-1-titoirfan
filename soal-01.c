e17e2f7a064a0e2a56d2b03e1e679d84d7036bc6
Irfan Tito Kurniawan <42381907+titoirfan@users.noreply.github.com>
2021-02-28 17:35:22 +0700

    Update soal-01.c

--- soal-01.c
+++ soal-01.c
@@ -1,60 +1,77 @@
-/*  EL2208 Praktikum Pemecahan Masalah dengan C 2020/2021
-*   Modul            : 3
-*   Soal             : 1
-*   Hari dan Tanggal : 
-*   Nama (NIM)       :  
-*   Asisten (NIM)    : 
-*   Nama File        : soal-01.c
-*   Deskripsi        : 
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 
-//Untuk rotasi matriks
 void rotate(int degree, int matrix[3][3]){
-    //implementasi dengan akses matriks menggunakan pointer
+    int temp, loop;
+    loop=degree/90;
+    for(int i=0; i<loop; i++){
+        temp =  *(*(matrix + 0) + 0); 
+        *(*(matrix + 0) + 0) = *(*(matrix + 2) + 0); 
+        *(*(matrix + 2) + 0) = *(*(matrix + 2) + 2); 
+        *(*(matrix + 2) + 2) = *(*(matrix + 0) + 2); 
+        *(*(matrix + 0) + 2) = temp; 
+
+        temp = *(*(matrix + 0) + 1); 
+        *(*(matrix + 0) + 1) = *(*(matrix + 1) + 0); 
+        *(*(matrix + 1) + 0) = *(*(matrix + 2) + 1);  
+        *(*(matrix + 2) + 1) = *(*(matrix + 1) + 2); 
+        *(*(matrix + 1) + 2) = temp; 
+    }
 }
 
-//Untuk swap value
 void swap(int *a, int *b){
-    //Implementasi
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
-//Untuk mirror sumbu y
 void mirror_y(int matrix[3][3]){
-    //implementasi dengan akses matriks menggunakan pointer
+    for(int i=0; i<3; i++){
+        swap((*(matrix + i) + 0), (*(matrix + i) + 2));
+    }
 }
 
-//untuk mirror sumbu x
 void mirror_x(int matrix[3][3]){
-    //implementasi dengan akses matriks menggunakan pointer
+    for(int i=0; i<3; i++){
+        swap((*(matrix + 0) + i), (*(matrix + 2) + i));
+    }
 }
 
-//main
 int main(){
-    //Inisiasi matriks
-    int  matrix[3][3];
-
-    //Print untuk input elemen matriks
+    int  matrix[3][3], temp, degree, deg;
+    char c;
     printf("Masukkan input elemen matriks:\n");
-
-    //Print untuk input sudut rotasi
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            scanf("%d", (*(matrix + i) + j));
+        }
+    }
     printf("Masukkan sudut rotasi: ");
-
-    //Print ketika kondisi sudut rotasi bukan kelipatan 90 derajat
-    printf("Sudut rotasi bukan kelipatan 90 derajat!\n");
-    
-    //Print untuk input sumbu mirror
-    printf("Masukkan sumbu mirror: ");
-    
-    //Print ketika mirror bukan sumbu x atau y
-    printf("Input sumbu mirror bukan x atau y!");
-
-    //Print output matriks rotasi dan mirror
-    // printf("Output matriks rotasi %d derajat dan mirror sumbu %c:\n", degree, sumbu);
-
-    //Print matriks dengan akses matriks menggunakan pointer
-
+    scanf("%d", &degree);
+    deg = degree;
+    if(degree % 90 == 0){
+        rotate(degree, matrix);
+        printf("Masukkan sumbu mirror: ");
+        scanf("%s", &c);
+        if (c=='x'){
+            mirror_x(matrix); 
+        }
+        else if (c=='y'){
+            mirror_y(matrix); 
+        } else{
+            printf("Input sumbu mirror bukan x atau y!");
+            return 0;
+        }
+        printf("Output matriks rotasi %d derajat dan mirror sumbu %c:\n", deg, c);
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                printf("%d ", *(*(matrix + i) + j));
+            }
+            printf("\n");
+        }
+    } else{
+        printf("Sudut rotasi bukan kelipatan 90 derajat!\n");
+    }
     return 0;
 }
